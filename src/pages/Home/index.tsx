d750efccb525a3046c5af726cd7194cdd82fc27a
ThiagoInstivo <thiago.fernandes@insitvo.com.br>
2023-02-11 02:29:15 -0300

    feat: add icon location:

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -12,11 +12,12 @@ export default function HomeScreen() {
   const [latitude, setLatitude] = useState(-20.398259);
   const [longitude, setLongitude] = useState(-43.507726); //utilizaremos estas duas variáveis (latitude e longitude) como posições padrão caso não seja possível obter a posição do usuário.
 
-  const {coords, errorMsg} = useLocation(); //utilizando o hook que vai nos fornecer a posição do usuário.
+  const {coords, errorMsg} = useLocation();
+  console.log('coors', coords); //utilizando o hook que vai nos fornecer a posição do usuário.
   return (
     <MapView
       showsUserLocation={true} //destacando a localização do usuário no mapa
-      showsMyLocationButton={false} //ocultando o botão que move o mapa para a localização do usuário
+      showsMyLocationButton={true} //ocultando o botão que move o mapa para a localização do usuário
       toolbarEnabled={false} //ocultando opções do google maps ao clicar em objetos do mapa
       style={{
         height: '100%',
