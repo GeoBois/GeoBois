a439e207e07071e7b9f2bc3914446275fef31e79
DicousInstivo22 <joao.victor@instivo.com.br>
2023-02-11 17:47:27 -0300

    feat(navigator): rotas

--- App.tsx
+++ App.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import HomeScreen from './src/pages/Home/index';
 import {enableLatestRenderer} from 'react-native-maps';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { BemVindo } from './src/pages/BemVindo';
 
 enableLatestRenderer();
 
+const Stack = createNativeStackNavigator();
+function Router() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="home">
+        <Stack.Screen name="login" component={BemVindo} />
+        <Stack.Screen name="home"  component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
 function App() {
-  return <HomeScreen />;
+  return <Router />;
 }
 
 export default App;
